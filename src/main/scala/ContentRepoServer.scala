ab067e45048a9fa95189f4d05db715e3aefb0f0e
Greg Heartsfield <scsibug@imap.cc>
2011-09-29 21:17:18 -0500

    HTTP port taken from config file.

--- src/main/scala/ContentRepoServer.scala
+++ src/main/scala/ContentRepoServer.scala
@@ -3,7 +3,7 @@ import unfiltered.response._
 import org.apache.commons.configuration.{HierarchicalINIConfiguration}
 
 object ContentRepoServer {
-
+  var port = 8089
   // Echo Plan
   val echo = unfiltered.filter.Planify {
     case Path(Seg(p :: Nil)) => ResponseString(p)
@@ -13,13 +13,14 @@ object ContentRepoServer {
     println("Starting server")
     println("Reading Configuration")
     readConfiguration
-    unfiltered.jetty.Http.anylocal.filter(echo).run() 
+    unfiltered.jetty.Http.local(port).filter(echo).run() 
   }
 
   def readConfiguration() {
     val hic = new HierarchicalINIConfiguration("content_repo.config")
     println(hic.toString())
-    println("HTTP port: "+hic.getString("http.port"))
+    println("HTTP port: "+hic.getString("http.port"))  
+    port = hic.getInt("http.port");
   }
 
 }
