e759fae424a4a7dbadfb3cd8e6517307d764ea4a
Greg Heartsfield <scsibug@imap.cc>
2011-10-02 23:27:40 -0500

    Setup Config/RedisStore objects.

--- src/main/scala/ContentRepoServer.scala
+++ src/main/scala/ContentRepoServer.scala
@@ -2,33 +2,31 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.jetty._
 import unfiltered.scalate._
-import org.apache.commons.configuration.{HierarchicalINIConfiguration}
 import org.slf4j._
 
 object ContentRepoServer {
   var port = 8089
 
   def main(args: Array[String]) {
-    println("Starting server")
-    println("Reading Configuration")
-    readConfiguration
+    println("Starting server on port "+Config.httpPort)
+    // Setup redis clients
+    RedisStore.createStore("cache",
+                           Config.redisHost,
+                           Config.redisPort,
+                           Config.redisSessionDB)
+    RedisStore.createStore("main",
+                           Config.redisHost,
+                           Config.redisPort,
+                           Config.redisMainDB)
     // Configure AuthPlan
     var authplan = new AuthPlan
     // Order of plan evaluation
     var plans = Seq(new RootPlan,
                     authplan)
     def applyPlans = plans.foldLeft(_: Server)(_ filter _)
-    applyPlans(Http(port)
+    applyPlans(Http(Config.httpPort)
                .context("/static") {
                  _.resources(getClass().getResource("/static/"))
                }).run()
   }
-
-  def readConfiguration() {
-    val hic = new HierarchicalINIConfiguration("content_repo.config")
-    println(hic.toString())
-    println("HTTP port: "+hic.getString("http.port"))  
-    port = hic.getInt("http.port");
-  }
-
 }
