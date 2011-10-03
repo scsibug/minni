f14df762262dc8979222a666660ff89329a1a1e4
Greg Heartsfield <scsibug@imap.cc>
2011-10-02 23:27:21 -0500

    Redis client management obj.

---
+++ src/main/scala/RedisStore.scala
@@ -0,0 +1,18 @@
+import com.redis._
+import scala.collection.mutable.{Map}
+object RedisStore {
+  private val stores = Map[String, RedisClient]()
+
+  def getStore(id: String) {
+    stores get id
+  }
+
+  def createStore(name: String, host: String, port: Int, database: Int) {
+    val client = new RedisClient(host,port)
+    client.select(database)
+    RedisStore.synchronized {
+      stores += (name -> client)
+    }
+    client
+  }
+}
