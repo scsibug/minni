5c279c7d4a79e5f727bbf900aa3664ecb190e92f
Greg Heartsfield <scsibug@imap.cc>
2011-10-05 23:58:17 -0500

    More intelligent handling of authenticated and unauthenticated requests to /users.

--- src/main/scala/com/gregheartsfield/minni/UserPlan.scala
+++ src/main/scala/com/gregheartsfield/minni/UserPlan.scala
@@ -14,12 +14,17 @@ class UserPlan extends Plan {
   val logger = LoggerFactory.getLogger(classOf[UserPlan])
 
   def intent = {
-    case req@GET(Path(Seg("users" :: username :: Nil))) & Authed(auth_user) => {
-      if (username == auth_user)
-        Ok ~>ResponseString("You are authenticated as "+auth_user)
-      else
-        Unauthorized ~> ResponseString("Please leave")
-      //Ok ~> Scalate(req, "user.scaml")
+    case req@GET(Path(Seg("users" :: username :: Nil))) => {
+      Authed(req) match {
+        case Some(auth_user) =>
+          if (username == auth_user)
+            Ok ~>ResponseString("You are authenticated as "+auth_user)
+            //Ok ~> Scalate(req, "user.scaml")
+          else
+            Unauthorized ~> ResponseString("Please leave")
+        case _ =>
+          Redirect("/login")
+      }
     }
   }
 }
